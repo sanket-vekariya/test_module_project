0bd7322cadec96fe98770c831ce70ed0be561b99
sanket-vekariya-sa <sanket.vekariya.sa@gmail.com>
2019-12-24 19:17:02 +0530

    [fix] 3 modules with inter-communication

--- test_second_module/src/main/java/com/sa/test_second_module/MainSecondActivity.kt
+++ test_second_module/src/main/java/com/sa/test_second_module/MainSecondActivity.kt
@@ -1,17 +1,20 @@
 package com.sa.test_second_module
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sa.test_third_module.MainThirdActivity
+import kotlinx.android.synthetic.main.activity_main_second.*
 
 class MainSecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_second)
-//
-//        mainSecondButton.setOnClickListener {
-//            val intent = Intent(this,MainAppActivity::class.java)
-//            startActivity(intent)
-//        }
+
+        mainSecondButton.setOnClickListener {
+            val intent = Intent(this, MainThirdActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ test_third_module/src/main/java/com/sa/test_third_module/MainThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.sa.test_third_module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainThirdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_third)
+    }
+}
