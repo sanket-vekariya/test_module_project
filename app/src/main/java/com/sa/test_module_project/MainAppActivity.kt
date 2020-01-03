3e70dd27d7109ff238a82ccb7d00a64ef7688fe0
Rudraksh-Shuklasa <rudraksh.shukla.sa@gmail.com>
2020-01-03 11:51:51 +0530

    Final commit after all test case check

--- app/src/main/java/com/sa/test_module_project/MainAppActivity.kt
+++ app/src/main/java/com/sa/test_module_project/MainAppActivity.kt
@@ -3,6 +3,7 @@ package com.sa.test_module_project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.sa.test_second_module.MainSecondActivity
 import com.sa.test_third_module.MainThirdActivity
 import kotlinx.android.synthetic.main.activity_main_first.*
@@ -12,6 +13,10 @@ class MainAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_first)
+
+        Glide.with(this)
+            .load("https://picsum.photos/200/300")
+            .into(image);
         mainFirstButton.setOnClickListener {
             val intent = Intent(this, MainSecondActivity::class.java)
             startActivity(intent)
